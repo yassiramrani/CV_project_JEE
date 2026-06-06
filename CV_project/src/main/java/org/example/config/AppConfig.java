1aba082b0256b6fa7c6fce2348c6c2698d12f390
yassiramrani04 <amraniyassir04@gmail.com>
2026-06-06 17:05:55 +0100

    phase 0 completed

---
+++ CV_project/src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,13 @@
+package org.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "org.example")
+public class AppConfig implements WebMvcConfigurer {
+    // Web-specific configurations (like CORS for React) will go here later
+}
